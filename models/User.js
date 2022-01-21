f604996d1ff9e874ed02d51234f2b6479afaaa63
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-21 14:04:45 -0600

    added user model and index.js

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: 'Name is Required',
+    trim: true
+  },
+
+    email: {
+    type: String,
+    required: 'Email is Required',
+    unique: true,
+    match: [/.+@.+\..+/]
+  },
+
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  
+  friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+      }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+  );
+
+// get total count of friends
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});  
+
+const User = model('User', UserSchema);
+
+module.exports = User;
